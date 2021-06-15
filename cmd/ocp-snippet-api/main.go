e8e6a337e2667b116bfc712a5c3447a6a9ce12ba
olegus <kilogran18@gmail.com>
2021-06-15 22:46:19 +0300

    Task-7 fix
    Дозаливаю main.go (не хотел этого делать из-за пароля, но так код получается не рабочим придётся залить...)

--- cmd/ocp-snippet-api/main.go
+++ cmd/ocp-snippet-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
 	"log"
@@ -13,6 +14,7 @@ import (
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	api "github.com/ozoncp/ocp-snippet-api/internal/api"
+	"github.com/ozoncp/ocp-snippet-api/internal/repo"
 	desc "github.com/ozoncp/ocp-snippet-api/pkg/ocp-snippet-api"
 )
 
@@ -26,14 +28,46 @@ var (
 	httpEndpoint = fmt.Sprintf("localhost:%d", httpPort)
 )
 
+func createDB() *sql.DB {
+	const (
+		dsnPreffix string = "postgres://"
+		host       string = "localhost"
+		port       int    = 5432
+		name       string = "postgres"
+		user       string = "postgres"
+		pswd       string = "leshiy"
+	)
+
+	dsn := fmt.Sprintf("%s%s:%s@%s", dsnPreffix, user, pswd, host)
+	if port >= 0 {
+		dsn = fmt.Sprintf("%s:%d", dsn, port)
+	}
+	dsn = fmt.Sprintf("%s/%s", dsn, name)
+	fmt.Println(dsn)
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatalf("Failed to open DB: %v\n", err)
+		return nil
+	}
+
+	return db
+}
+
 func run() error {
 	listener, err := net.Listen("tcp", grpcEndpoint)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	ctx := context.Background()
 	s := grpc.NewServer()
-	desc.RegisterOcpSnippetApiServer(s, api.NewOcpSnippetApi())
+
+	db := createDB()
+	repo := repo.NewRepoDB(db, ctx)
+	defer db.Close()
+
+	desc.RegisterOcpSnippetApiServer(s, api.NewOcpSnippetApi(repo))
 
 	go func() {
 		fmt.Printf("GRPC server listening on %s\n", grpcEndpoint)
@@ -44,7 +78,7 @@ func run() error {
 	}()
 
 	runtimeMux := runtime.NewServeMux()
-	if err = desc.RegisterOcpSnippetApiHandlerFromEndpoint(context.Background(), runtimeMux, grpcEndpoint, []grpc.DialOption{grpc.WithInsecure()}); err != nil {
+	if err = desc.RegisterOcpSnippetApiHandlerFromEndpoint(ctx, runtimeMux, grpcEndpoint, []grpc.DialOption{grpc.WithInsecure()}); err != nil {
 		log.Fatalf("failed to register handler: %v", err)
 	}
 
@@ -61,4 +95,5 @@ func main() {
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
+
 }
